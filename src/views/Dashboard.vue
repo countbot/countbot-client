86227a4ce2fe2eca75aee7763f534a4e4cba2b1c
dober <doberste8@gmail.com>
2019-03-08 05:22:07 +0000

    Add avg char count & animate loading screen.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,11 +3,21 @@
     <div class="row">
       <div class="col text-center">
         <h1>Werewolves Dashboard</h1>
+      </div>
+    </div>
+    <div
+      v-if="cf.count-1"
+      class="row"
+    >
+      <div class="col text-center">
         <p v-if="cf.cf.groupAll().value() === cf.cf.size()">
           {{ cf.cf.size() }} messages
         </p>
         <p v-else>
           {{ cf.cf.groupAll().value() }} of {{ cf.cf.size() }} messages selected
+        </p>
+        <p>
+          {{ charCount }} chars per message (avg)
         </p>
         <button
           type="button"
@@ -23,6 +33,17 @@
         >
           Sort Users
         </button>
+      </div>
+    </div>
+    <div
+      v-else
+      class="text-center row align-items-center"
+      style="height: 20em;"
+    >
+      <div class="col">
+        <h2 class="loading">
+          Loading<span>.</span><span>.</span><span>.</span>
+        </h2>
       </div>
     </div>
     <div
@@ -137,12 +158,6 @@
         <UserList title="User List" />
       </div>
     </div>
-    <div
-      v-else
-      class="text-center"
-    >
-      <h2>Loading...</h2>
-    </div>
   </div>
 </template>
 
@@ -207,6 +222,12 @@ export default {
       return d3.scaleBand()
         .domain(this.cf.roleGroup.top(Infinity).map(d => d.key).sort())
         .padding(0.1);
+    },
+    charCount() {
+      // eslint-disable-next-line no-unused-vars
+      const c = this.cf.count; // Used to force recalulation
+      return Math.round(this.cf.cf.allFiltered().map(d => (d.te ? d.te.length : 0))
+        .reduce((acc, curr) => acc + curr, 0) / this.cf.cf.groupAll().value());
     },
   },
   created() {
@@ -304,4 +325,69 @@ export default {
     cursor: pointer;
     text-decoration: underline !important;
   }
+
+@keyframes blink {
+    /**
+     * At the start of the animation the dot
+     * has an opacity of .2
+     */
+    0% {
+      opacity: .2;
+    }
+    /**
+     * At 20% the dot is fully visible and
+     * then fades out slowly
+     */
+    20% {
+      opacity: 1;
+    }
+    /**
+     * Until it reaches an opacity of .2 and
+     * the animation can start again
+     */
+    100% {
+      opacity: .2;
+    }
+}
+
+.loading span {
+    /**
+     * Use the blink animation, which is defined above
+     */
+    animation-name: blink;
+    /**
+     * The animation should take 1.4 seconds
+     */
+    animation-duration: 1.4s;
+    /**
+     * It will repeat itself forever
+     */
+    animation-iteration-count: infinite;
+    /**
+     * This makes sure that the starting style (opacity: .2)
+     * of the animation is applied before the animation starts.
+     * Otherwise we would see a short flash or would have
+     * to set the default styling of the dots to the same
+     * as the animation. Same applies for the ending styles.
+     */
+    animation-fill-mode: both;
+}
+
+.loading span:nth-child(2) {
+    /**
+     * Starts the animation of the third dot
+     * with a delay of .2s, otherwise all dots
+     * would animate at the same time
+     */
+    animation-delay: .2s;
+}
+
+.loading span:nth-child(3) {
+    /**
+     * Starts the animation of the third dot
+     * with a delay of .4s, otherwise all dots
+     * would animate at the same time
+     */
+    animation-delay: .4s;
+}
 </style>
